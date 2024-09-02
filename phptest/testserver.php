f7943b96647d77bba8792259c684efd2674b74a7
Carl1713 <88369334+Carl1713@users.noreply.github.com>
2024-09-03 00:37:52 +0800

    test php codes

---
+++ phptest/testserver.php
@@ -0,0 +1,11 @@
+<?php
+echo "Hellow";
+/*if($_SERVER["REQUEST_METHOD"]=="POST")
+{
+$watts = htmlspecialchars($_POST["watts"]);
+$hours = htmlspecialchars($_POST["hours"]);
+$price = htmlspecialchars($_POST["price"]);   
+
+
+echo "$watts";
+}*/
