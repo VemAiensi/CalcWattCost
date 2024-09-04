d6d9ba675d282853b0ce2a10999080c66a2447fb
Vem Aiensi <vem.aiensi@gmail.com>
2024-09-04 23:37:39 +0800

    Update header.php
    Added body tag

--- phptest/header.php
+++ phptest/header.php
@@ -7,19 +7,17 @@
     <link rel="stylesheet" href="test.css">
     
 </head>
-
-<header>
-
-<div class = "topnav">
-    <a href="testUI.php">Home</a>
-    <a href="#">Portfolio</a>
-    <a href="#">About</a>
-    <a href="#">Contact</a>
-    <a href="">Login</a>
-    <a href="#">Signup</a>
-
-</div>
-</header>
-
-
+<body>
+    <header>
+        <div class = "topnav">
+            <a href="testUI.php">Home</a>
+            <a href="#">Portfolio</a>
+            <a href="#">About</a>
+            <a href="#">Contact</a>
+            <a href="">Login</a>
+            <a href="#">Signup</a>
+        </div>
+    </header>
+</body>
+    
 </html>
