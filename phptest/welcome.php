193fb2e56759600f4acb0ee86f57fd2fb5b36ca0
Vem Aiensi Marasigan <vem.aiensi@gmail.com>
2024-09-05 06:48:03 +0800

    just an update

--- phptest/welcome.php
+++ phptest/welcome.php
@@ -1,7 +1,7 @@
 <html>
 <body>
 <?
-name = $email = $gender = $comment = $website = "";
+$name = $email = $gender = $comment = $website = "";
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
   $name = test_input($_POST["name"]);
